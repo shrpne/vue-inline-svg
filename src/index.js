2313a7029afe7755ab8d088844c650598ab30da4
Aimo Künkel <mail@aimo.cc>
2019-08-22 12:52:52 +0200

    changed spacing

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const InlineSvgComponent = {
         return createElement(
             'svg',
             {
-            	on: this.$listeners,
+                on: this.$listeners,
                 attrs: Object.assign(this.svgAttrs, filterAttrs(this.$vnode.data.attrs)),
             },
         );
